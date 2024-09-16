a2e99ac87a7376b5cee4a29e02c532dc2db22cc8
Andrés Castro <andrescn20@gmail.com>
2024-09-16 09:16:31 -0600

    Reacomodo de Códigos.

---
+++ Código/Python/read_serial.py
@@ -0,0 +1,14 @@
+# Este script lee la información que se envía por el puerto serial y la imprime en la consola.
+# La intención es tener acceso a la información enviada por el dispositivo y así leer los 
+# mensajes que este imprime. 
+
+import serial
+from datetime import datetime
+
+ser = serial.Serial('COM8', 9600) # Cambiar el puerto COM8 por el puerto al que está conectado el dispositivo. 
+# El puerto correcto varía entre computadoras. 
+while True:
+    if ser.in_waiting > 0:
+        line = ser.readline().decode('utf-8').strip()
+        if line:
+            print(line)
